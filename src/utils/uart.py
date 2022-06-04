f3259a5ad9d08b025f85c80acb10c92df035d6b9
Tim Hendriks <tim-hendriks@hotmail.de>
2022-06-04 20:44:43 +0200

    restore previous frame jump handling

--- src/utils/uart.py
+++ src/utils/uart.py
@@ -1,5 +1,6 @@
 import sys
 import glob
+import time
 import serial
 import bpy
 
@@ -17,23 +18,22 @@ COMMAND_TYPE_FRAME_JUMP = 2
 class UartController:
     serial_ports = []
     serial_connection = None
-    position_log = {}
+    frame = None
+    positions = {}
 
     def on_frame_change_post(self, scene):
         if not self.is_connected():
             return
 
-        for pose_bone in get_active_pose_bones(scene):
-            bone = pose_bone.bone
-            previous_position = self.position_log.get(bone.name)
-            position, in_range = calculate_position(pose_bone, None)
+        frame_jump = self.frame is not None and abs(
+            scene.frame_current - self.frame) > 10
 
-            if not in_range or previous_position == position:
-                continue
+        if frame_jump:
+            self.handle_frame_jump(scene)
+        else:
+            self.handle_default(scene)
 
-            self.send_position(
-                pose_bone.bone.servo_settings.servo_id, position)
-            self.position_log[bone.name] = position
+        self.frame = scene.frame_current
 
     def send_position(self, servo_id, position):
         command = [COMMAND_START, COMMAND_TYPE_MOVE_SERVO, servo_id]
@@ -42,8 +42,55 @@ class UartController:
 
         try:
             self.serial_connection.write(command)
+            self.positions[servo_id] = position
         except serial.SerialException:
             self.close_serial_connection()
+
+    def handle_default(self, scene):
+        for pose_bone in get_active_pose_bones(scene):
+            bone = pose_bone.bone
+            servo_id = bone.servo_settings.servo_id
+            previous_position = self.positions.get(servo_id)
+            position, in_range = calculate_position(pose_bone, None)
+
+            if not in_range or previous_position == position:
+                continue
+
+            self.send_position(servo_id, position)
+
+    def handle_frame_jump(self, scene):
+        diffs = []
+        target_positions = {}
+
+        for pose_bone in get_active_pose_bones(scene):
+            target_position, in_range = calculate_position(pose_bone, None)
+            if in_range:
+                servo_id = pose_bone.bone.servo_settings.servo_id
+                target_positions[servo_id] = target_position
+                diffs.append(abs(target_position - self.positions[servo_id]))
+
+        steps = max(diffs)
+
+        if bpy.context.screen.is_animation_playing:
+            bpy.ops.screen.animation_cancel(restore_frame=False)
+
+        window_manager = bpy.context.window_manager
+        window_manager.progress_begin(0, steps)
+
+        for step in range(steps):
+            window_manager.progress_update(step)
+            for servo_id, target_position in target_positions.items():
+                previous_position = self.positions[servo_id]
+                if target_position == previous_position:
+                    continue
+                if target_position > previous_position:
+                    new_position = previous_position + 1
+                else:
+                    new_position = previous_position - 1
+                self.send_position(servo_id, new_position)
+            time.sleep(.01)
+
+        window_manager.progress_end()
 
     def scan_serial_ports(self):
         self.serial_ports.clear()
