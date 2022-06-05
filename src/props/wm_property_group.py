d444c434e1a9bcb98f7d5a095e03af6c13e74526
Tim Hendriks <tim-hendriks@hotmail.de>
2022-06-05 12:11:48 +0200

    close serial connection when changing port or baud

--- src/props/wm_property_group.py
+++ src/props/wm_property_group.py
@@ -14,28 +14,36 @@ def get_serial_port_items(_self, _context):
     return items
 
 
-class WindowManagerPropertyGroup(PropertyGroup):
-    def update_serial_connection(self, context):
-        UART_CONTROLLER.close_serial_connection()
-        if self.live_mode is False or self.serial_port == '':
-            return
-        if UART_CONTROLLER.open_serial_connection() is False:
-            self.live_mode = False
-        UART_CONTROLLER.on_frame_change_post(context.scene)
+def close_serial_connection(_self, _context):
+    UART_CONTROLLER.close_serial_connection()
 
+
+def toggle_serial_connection(self, context):
+    close_serial_connection(self, context)
+
+    if not self.live_mode or self.serial_port == '':
+        return
+
+    if UART_CONTROLLER.open_serial_connection():
+        UART_CONTROLLER.on_frame_change_post(context.scene)
+    else:
+        self.live_mode = False
+
+
+class WindowManagerPropertyGroup(PropertyGroup):
     live_mode: bpy.props.BoolProperty(
         name="Live Mode",
-        update=update_serial_connection,
+        update=toggle_serial_connection,
         description="Send UART commands on each frame change"
     )
     serial_port: bpy.props.EnumProperty(
         name="Serial Port",
-        update=update_serial_connection,
+        update=close_serial_connection,
         items=get_serial_port_items
     )
     baud_rate: bpy.props.EnumProperty(
         name="Baud Rate",
-        update=update_serial_connection,
+        update=close_serial_connection,
         default="115200",
         items=[
             ("19200", "19200", ""),
