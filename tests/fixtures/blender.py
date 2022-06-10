f7c57eb0a4b04f7d18267724e6ceb60e897bfc2c
Tim Hendriks <tim-hendriks@hotmail.de>
2022-06-11 01:19:23 +0200

    use blender python console with pexpect

--- tests/fixtures/blender.py
+++ tests/fixtures/blender.py
@@ -1,20 +1,42 @@
 import os
 import pytest
+import pexpect
+
 
 class Blender:
     file = None
-    script = None
+    cwd = None
+    spawn = None
+    timeout = 3
+
+    def __init__(self):
+        self.cwd = os.getcwd()
+
+    def tmp(self, path):
+        return self.cwd + "/" + path
 
     def set_file(self, path):
-        self.file = path
+        self.file = self.cwd + "/" + path
 
-    def set_script(self, name):
-        self.script = f"tests/blender/{name}"
+    def start(self):
+        command = f"blender {self.file} --background --python-console"
+        self.spawn = pexpect.spawn(command, timeout=self.timeout)
 
-    def run(self):
-        args = f"--background --python-exit-code 1 --python {self.script}"
-        command = f"blender {self.file} {args}"
-        return os.system(command)
+    def send_lines(self, lines):
+        sent_bytes = 0
+        for line in lines:
+            sent_bytes += self.send_line(line)
+        return sent_bytes
+
+    def send_line(self, line):
+        return self.spawn.sendline(line)
+
+    def expect(self, line):
+        return self.spawn.expect(line)
+
+    def close(self):
+        self.spawn.close()
+
 
 @pytest.fixture
 def blender():
---
+++ tests/integration/test_export.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+
+def test_json_export(blender):
+    export_file = blender.tmp("/tests/tmp/test.json")
+
+    if os.path.exists(export_file):
+        os.remove(export_file)
+
+    assert not os.path.exists(export_file)
+
+    blender.set_file("examples/Simple/example.blend")
+    blender.start()
+    blender.send_lines([
+        "import bpy",
+        f"bpy.ops.export_anim.servo_positions_json(filepath='{export_file}')"
+    ])
+    blender.expect("{'FINISHED'}")
+    blender.close()
+
+    assert os.path.exists(export_file), "expected export file to be present"
+
+    with open(export_file, 'r', encoding='utf-8') as file:
+        parsed = json.load(file)
+
+    assert parsed["file"] == "example.blend"
+    assert parsed["frames"] == 100
+    assert len(parsed["positions"]["Bone"]) == 100
+    assert parsed["positions"]["Bone"][0] == "90"
+    assert parsed["positions"]["Bone"][32] == "45"
+    assert parsed["positions"]["Bone"][65] == "135"
+    assert parsed["positions"]["Bone"][99] == "90"
+
+    os.remove(export_file)
+
+
+def test_json_arduino(blender):
+    export_file = blender.tmp("/tests/tmp/test.h")
+
+    if os.path.exists(export_file):
+        os.remove(export_file)
+
+    assert not os.path.exists(export_file)
+
+    blender.set_file("examples/Simple/example.blend")
+    blender.start()
+    blender.send_lines([
+        "import bpy",
+        f"bpy.ops.export_anim.servo_positions_arduino(filepath='{export_file}')"
+    ])
+    blender.expect("{'FINISHED'}")
+    blender.close()
+
+    assert os.path.exists(export_file), "expected export file to be present"
+
+    os.remove(export_file)
