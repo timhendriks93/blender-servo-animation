417e042c0dfefb39fe2b4aacb69f388f5776aab8
Tim Hendriks <tim-hendriks@hotmail.de>
2022-06-11 01:19:23 +0200

    use blender python console with pexpect

--- tests/blender/servo_settings_active.py
+++
@@ -1,9 +0,0 @@
-import bpy
-
-POSE_BONE = bpy.context.active_pose_bone
-
-assert POSE_BONE is not None
-
-bone = POSE_BONE.bone
-
-assert bone.servo_settings.active is True
--- tests/integration/test_servo_settings_active.py
+++
@@ -1,5 +0,0 @@
-def test_servo_settings_active(blender):
-    blender.set_file("examples/Simple/example.blend")
-    blender.set_script("servo_settings_active.py")
-
-    assert blender.run() == 0, "expected status code to be 0"
