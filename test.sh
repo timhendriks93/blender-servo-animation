348dff97284c2e6d11a4e68f9e4cbf2f17daa538
Tim Hendriks <tim-hendriks@hotmail.de>
2023-04-18 19:22:11 +0200

    allow blender to use system python env for tests

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ blender \
     -noaudio \
     --background examples/Simple/simple.blend \
     --addons servo_animation \
+    --python-use-system-env \
+    --python-exit-code 1 \
     --python tests/main.py
 
 exit $?
